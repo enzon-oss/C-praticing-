f0ba8eb94462f3f44212305c11dbc46570e31c64
enzon-oss <enzomanoel.c.f@gmail.com>
2025-08-17 20:19:38 -0300

    (1)Temperatura.cpp

--- (1)Temperatura.cpp
+++ (1)Temperatura.cpp
@@ -7,12 +7,13 @@ int main(){
     float fahr;
 
     printf("Para saber a temperatura em Fahrenheit, coloque a temperatura em graus celsius");
-    scanf("%d",&celsius);
+    scanf("%d",&cel);
 
 
-        fahr = (celsius * 1.8) + 32
+        fahr = (cel * 1.8) + 32
 
-    printf("Sua temperatura em graus fahrenheit e%f",fahr);
+    printf("Sua temperatura em graus fahrenheit e %f",fahr);
 
     return 0;
+
 }
