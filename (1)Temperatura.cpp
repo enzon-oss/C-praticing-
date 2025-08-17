33278c593b93048e8e04b937c42b452fb5ae088c
enzon-oss <enzomanoel.c.f@gmail.com>
2025-08-17 19:17:25 -0300

    Add files via upload
    Correção, eu mandei o arquivo errado na outra pasta, esse aqui é o certo

---
+++ (1)Temperatura.cpp
@@ -0,0 +1,18 @@
+// Questao 1- Temperatura//
+#include<stdio.h>
+
+int main(){
+
+    int cel;
+    float fahr;
+
+    printf("Para saber a temperatura em Fahrenheit, coloque a temperatura em graus celsius");
+    scanf("%d",&celsius);
+
+
+        fahr = (celsius * 1.8) + 32
+
+    printf("Sua temperatura em graus fahrenheit e%f",fahr);
+
+    return 0;
+}
